b56fab3711548829f9cc0589e73313fcb3b7347b
pro100 <frostymag177@gmail.com>
2022-01-31 21:13:15 +0300

    Initial commit

---
+++ Homeworkp2l5Application.java
@@ -0,0 +1,13 @@
+package java.course.homeworkp2l5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homeworkp2l5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homeworkp2l5Application.class, args);
+	}
+
+}
